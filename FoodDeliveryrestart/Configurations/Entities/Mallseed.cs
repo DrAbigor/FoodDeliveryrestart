d505cead1dbf48facc94cc7659fbb561a5a52c57
Eddri <Eddri@LAPTOP-HLNRCKDD>
2026-01-08 14:20:20 +0800

    add MallSeed

---
+++ FoodDeliveryrestart/Configurations/Entities/Mallseed.cs
@@ -0,0 +1,31 @@
+﻿using FoodDeliveryrestart.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace FoodDeliveryrestart.Configurations.Entities
+{
+    public class Mallseed : IEntityTypeConfiguration<Mall>
+    {
+        public void Configure(EntityTypeBuilder<Mall> builder)
+        {
+            new Mall
+            {
+                Id = 1,
+                MallName = "Tampines Mall",
+                Address = "4 Tampines Central 5, Singapore 529510",
+            };
+            new Mall
+            {
+                Id = 2,
+                MallName = "Vivo City",
+                Address = "1 HarbourFront Walk, Singapore 098585",
+            };
+            new Mall
+            {
+                Id = 3,
+                MallName = "Orchard Central",
+                Address = "181 Orchard Rd, Singapore 238896"
+            };
+        }
+    }
+}
